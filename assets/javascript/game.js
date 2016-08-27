1a12d5bdb394974af844f310506ec455fdbe4192
Miguel Mejares <Miguel Mejares>
2016-08-27 18:01:57 -0500

    site push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ var number = (Math.floor(Math.random()*101) +19);
 
 
 $(document).ready(function(){
+
 	
 	$('#randomNumber').text(number);
 
@@ -19,42 +20,56 @@ $(document).ready(function(){
 
 	diamond_number = (Math.floor(Math.random()*11) + 1);
 
-	
 
 
 	$('#redRuby').click(function() {
 		score += redRuby_number;
 		$('#scoreNumber').text(score);
+		 checkScore();
 
 	});
 
 	$('#greenRuby').click(function() {
 		score += greenRuby_number;
 		$('#scoreNumber').text(score);
+		 checkScore();
 
 	});
 
 	$('#yellowGem').click(function() {
 		score += yellowGem_number;
 		$('#scoreNumber').text(score);
+		 checkScore();
 
 	});
 
 	$('#diamond').click(function() {
 		score += diamond_number;
 		$('#scoreNumber').text(score);
+		checkScore();
 
 	});
 
-	if(score == number) {
-		wins++;
-		$('#wins').text(wins);
-	}
-	else(score > number) {
-		losses++;
-		$('#losses').text(wins);
-		console.log(losses);
-	}
+
+	   function checkScore() {
+
+        if(score == number) {
+            wins++;
+            $('#wins').text(wins);
+            console.log('wins: '+ wins);
+        }
+        else if (score > number) {
+            losses++;
+            $('#losses').text(losses);
+            console.log('losses: ' + losses);
+            number = (Math.floor(Math.random()*101) +19);
+
+        }
+
+    }
+
+
+
 	
 
 	
